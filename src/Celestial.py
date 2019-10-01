a7187333519ecaad5bf8c5a66e725542412ac0c5
SindreP <sindrhpa@stud.ntnu.no>
2019-10-01 14:15:48 +0200

    Celestial bodies

---
+++ src/Celestial.py
@@ -0,0 +1,17 @@
+"""
+Created on Oct 01 2019
+@author: sindrhpa
+"""
+
+# Earth data
+earth_diameter_eqv = 12756.28*1000  # m
+earth_diameter_pol = 12713.56*1000  # m
+earth_mass = 5.9736*10**24  # kg
+
+# Moon data
+moon_diameter_eqv = 2*1738.14*1000  # m
+moon_mass = 7.3477*10**22  # kg
+moon_velocity = 1022  # m/s
+moon_orbit_period = 27.321  # days
+moon_periapsis = 362570*1000  # m
+moon_apoapsis = 405410*1000  # m
